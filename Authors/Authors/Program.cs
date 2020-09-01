3cbaf2ff4b02edfb4fcd167fc0d9faf59971a4f9
pupil <pupil@ivkhk.ee>
2020-09-01 15:06:00 +0300

    CityHotelApp is done

---
+++ Authors/Authors/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace AuthorApp
+{
+    class Program
+    {
+        public class Author{
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+
+            public override string ToString()
+            {
+                string str=$"Author: {Firstname}  {Lastname}";
+                return str;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Author[] authors =
+            {
+                new Author{Firstname="Andrjuxa",Lastname="Yspeh"},
+                new Author{Firstname="Andrjuxa",Lastname="Yspeh2"},
+                new Author{Firstname="Andrjuxa",Lastname="Yspeh3"},
+                new Author{Firstname="Andrjuxa",Lastname="Yspeh4"},
+            };
+            //....................for............................
+            Console.WriteLine("\nFor");
+            for (int i=0; i<authors.Length; i++)
+            {
+                Console.Write((i + 1)+$". {authors[i]}\n");
+            }
+
+            //.....................foreach........................
+            Console.WriteLine("\nForeach");
+            int n = 0;
+            foreach(Author author in authors)
+            {
+                n++;
+                Console.WriteLine($"\n{n}. {author}\n");
+            }
+            //....................................................
+            Console.WriteLine("\nLinQ");
+            var selectedAuthor = from author in authors
+                                 /*where author.Firstname=="Andrjuxa"
+                                 orderby author.Lastname descending*/
+                                 select author;//full array
+            n = 0;
+            foreach (Author author in selectedAuthor)
+            {
+                n++;
+                Console.WriteLine($"\n{n}. {author}\n");
+            }
+
+            //....................................................
+            Console.Write("Press any key.......");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CityHotelApp/Files/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace CityHotelApp
+{
+    //Muntjan Andrei PTVR18
+    class Program
+    {
+        public class Hotel
+        {
+            public string City { get; set; }
+            public string HotelName { get; set; }
+            public string Address { get; set; }
+            public string Services { get; set; }
+
+            public override string ToString()
+            {
+                return $"City:{City},Hotel: {HotelName},Address: {Address},Services: {Services}";
+            
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Hotel[] hotels =
+            {
+                new Hotel{City="Tallinn",HotelName="Inger Hotel",Address="Hotell sohtkohas Narva",Services="Wifi,Baar,Pool,Tasuta parkimine"},
+                new Hotel{City="Tallinn",HotelName="Metropol Spa Hotel",Address="Roseni 9",Services="Wifi,Baar,Pool,Tasuta parkimine,Works around the clock"},
+                new Hotel{City="Tallinn",HotelName="Art Hotell",Address="Lai 18",Services="Wifi,Baar,Pool,Tasuta parkimine,Restoraan,Peretoad"},
+                new Hotel{City="Tartu",HotelName="Metropol Spa Hotel",Address="Roseni 9",Services="Wifi,Baar,Pool,Tasuta parkimine,Works around the clock"},
+                new Hotel{City="Tartu",HotelName="Art Hotell",Address="Lai 18",Services="Wifi,Baar,Pool,Tasuta parkimine,Restoraan,Peretoad"},
+                new Hotel{City="Parnu",HotelName="Metropol Spa Hotel",Address="Roseni 9",Services="Wifi,Baar,Pool,Tasuta parkimine,Works around the clock"},
+                new Hotel{City="Parnu",HotelName="Art Hotell",Address="Lai 18",Services="Wifi,Baar,Pool,Tasuta parkimine,Restoraan,Peretoad"},
+                new Hotel{City="Toila",HotelName="Metropol Spa Hotel",Address="Roseni 9",Services="Wifi,Baar,Pool,Tasuta parkimine,Works around the clock"},
+            };
+            //For
+            Console.WriteLine("\nAll hotels:");
+            int n = 0;
+            foreach(Hotel hotel in hotels)
+            {
+                n++;
+                Console.WriteLine($"{n}. {hotel}");
+            }
+            //LinQ
+            Console.WriteLine("\nTallinn hotels:");
+            var selectedHotel = from hotel in hotels
+                                where hotel.City=="Tallinn"
+                                select hotel;
+            n = 0;
+            foreach(Hotel hotel in selectedHotel)
+            {
+                n++;
+                Console.WriteLine($"{n}. {hotel}");
+            }
+
+            //....................................................
+            Console.Write("Press any key.......");
+            Console.ReadKey();
+        }
+    }
+}
